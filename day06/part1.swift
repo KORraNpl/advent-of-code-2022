6ce94b3cf39e19878d31e38e77c5bfc93a3c346d
Remigiusz Żętkowski <remigiusz.zetkowski@gmail.com>
2022-12-06 19:43:26 +0100

    Day 6, because why not

---
+++ day06/part1.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let data: String
+
+do {
+    data = try String(contentsOfFile: "input.txt", encoding: .utf8)
+} catch {
+    print(error)
+    exit(1)
+}
+
+var s = data
+let markerLength = 4
+
+for i in 1...data.count - markerLength + 1 {
+    let lengthOfSet = Set(s.prefix(markerLength)).count
+    if lengthOfSet == markerLength {
+        print("Found marker at position \(i + markerLength - 1)")
+        break
+    }
+    s = String(s.dropFirst(1))
+}
